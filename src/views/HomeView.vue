3c1539fe8b7f2382bb653f9826efe198911e440e
RongMarin99 <rongmarin98@gmail.com>
2024-11-11 23:52:45 +0700

    search fuzzy by country name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@
   <div class="container">
     <div class="row p-3 bg-light mb-2 fixed-header" >
       <div class="col-4">
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            </form>
+        <form class="d-flex" @submit.prevent="getPaginatedData(all_data, filter.currentPage, filter.search)">
+          <input v-model="filter.search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
       </div>
       <div class="col-8 d-flex justify-content-end align-items-center">
         <div class="row">
@@ -58,11 +58,11 @@
           </div>
           
           <div class="card-body">
-            <h5 class="card-title">{{ item.name.common }}</h5>
+            <h5 class="card-title">{{ item.name.official }}</h5>
             <ul>
               <li>2 character Country Code: {{ item.cca2 }}</li>
               <li>3 character Country Code: {{ item.cca3 }}</li>
-              <li>Native Country Name: {{ item.name.nativeName?.eng?.official }}</li>
+              <li>Native Country Name: {{ (item.name.nativeName[item.cca3.toLowerCase()])?.official ?? item.name.nativeName?.eng?.official }}</li>
               <li>Alternative Country Name: 
                 <ul>
                     <li v-for="(row,i) in item.altSpellings" :key="i">
@@ -79,7 +79,7 @@
 
     <div class="row bg-light p-3">
       <div class="col-1">
-        <select class="form-select" v-model="pageSize">
+        <select class="form-select" v-model="filter.pageSize">
           <option v-for="(item, index) in paginationOption" :key="index" :value="item">
             {{ item }}
           </option>
@@ -104,12 +104,18 @@
 </div>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, reactive } from 'vue'
 import axios from 'axios'
 
-const pageSize = ref(25)
-const currentPage = ref(1);
+// const pageSize = ref(25)
+// const currentPage = ref(1);
+const filter = reactive({
+  pageSize: 25,
+  currentPage: 1,
+  search: ''
+})
 const lists = ref([])
+const all_data = ref([])
 const paginationOption = ref([
   25, 50, 75, 100
 ])
@@ -123,13 +129,23 @@ onMounted(() => {
 //function 
 const get = () => {
   axios.get("https://restcountries.com/v3.1/all?fields=name,flags,cca2,cca3,altSpellings,idd").then(response => {
-    lists.value = getPaginatedData(response.data, currentPage.value)
+    all_data.value = response.data
+    getPaginatedData(all_data.value, filter.currentPage)
   })
 }
 
-const getPaginatedData = (countries, page) => {
-  const start = (page - 1) * pageSize.value;
-  const end = start + pageSize.value;
-  return countries.slice(start, end);
+
+
+const getPaginatedData = (countries, page, searchTerm = '') => {
+  if(searchTerm){
+    lists.value = countries.filter(country => 
+      country.name.official.toLowerCase().includes(searchTerm.toLowerCase())
+    ); 
+    return
+  }
+
+  const start = (page - 1) * filter.pageSize;
+  const end = start + filter.pageSize;
+  lists.value = countries.slice(start, end);
 }
 </script>
